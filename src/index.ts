949fe9d01e7ef77567ccab16377999328b0c07a1
Erik Pastor <erik.pastor@glovoapp.com>
2021-02-13 13:16:27 +0100

    Codigo de ejemplo

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
-import Discord from 'discord.js';
+import Discord, { Channel, TextChannel } from 'discord.js';
 
 require('dotenv').config();
 
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user?.tag}!`);
-});
+let generalChannel: Channel | undefined, pruebaChannel: Channel | undefined;
 
-client.on('message', (msg) => {
-  if (msg.content === 'ping') {
-    msg.channel.send('pong');
-  }
+client.once('ready', () => {
+  console.log('Ready to rock!');
+  generalChannel = client.channels.cache.get('810098409729163267');
+  pruebaChannel = client.channels.cache.get('810108981028716545');
+
+  // Ejemplo de como responder a un mensaje
+  client.on('message', (message) => {
+    if (message.channel.id === '810098409729163267') {
+      if (message.content === 'Hola') message.channel.send('Que tal?');
+      if (message.content === 'Bien') message.channel.send('Me alegro!');
+    }
+  });
+
+  // Ejemplo de como enviar algo a un canal especifico
+  (pruebaChannel as TextChannel).send('Hola');
+  (generalChannel as TextChannel).send('Esto es general?');
 });
 
 client.login(process.env.discordBot);
